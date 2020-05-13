ee507b20109c8e9fda784dd001e91ad35115da5d
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-14 00:53:07 +0100

    Ups

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -30,18 +30,31 @@ public class MainActivity extends AppCompatActivity {
         String height_measure = spinnerHeight.getSelectedItem().toString().trim();
         String weight_measure = spinnerWeight.getSelectedItem().toString().trim();
 
-        double height = Double.parseDouble((editTextHeight.getText().toString()));
-        double weight = Double.parseDouble((editTextWeight.getText().toString()));
+        String heightValue = editTextHeight.getText().toString();
+        String weightValue = editTextWeight.getText().toString();
 
-        if (height_measure.equals("in")){
-            height = convertToCentimeter(height);
+        if (heightValue.length() != 0 && weightValue.length() != 0) {
+            double height = Double.parseDouble(heightValue);
+            double weight = Double.parseDouble(weightValue);
+
+            if (isHeightValid(height) && isWeightValid(weight)){
+                if (height_measure.equals("in")) {
+                    height = convertToCentimeter(height);
+                }
+                if (weight_measure.equals("lbs")) {
+                    weight = convertToKilogram(weight);
+                }
+                textViewBMI.setText(String.valueOf(calculateBMI(weight, height)));
+            } else if (!isWeightValid(height)){
+                editTextHeight.setError("Kindly enter a valid Height");
+            } else {
+                editTextWeight.setError("Kindly enter a valid Weight");
+            }
+        } else if (heightValue.length() == 0) {
+            editTextHeight.setError("Kindly enter a value for Height");
+        } else{
+            editTextWeight.setError("Kindly enter a value for Weight");
         }
-
-        if (weight_measure.equals("lbs")){
-            weight = convertToKilogram(weight);
-        }
-
-        textViewBMI.setText(String.valueOf(calculateBMI(weight, height)));
 
     }
 
@@ -61,5 +74,25 @@ public class MainActivity extends AppCompatActivity {
 
     private double calculateBMI(double weight, double height) {
         return (weight / Math.pow(height, 2));
+    }
+
+    private boolean isHeightValid(double height){
+        boolean status = false;
+
+        if (height > 0.5){
+            status = true;
+        }
+
+        return status;
+    }
+
+    private boolean isWeightValid(double weight){
+        boolean status = false;
+
+        if (weight > 5){
+            status = true;
+        }
+
+        return status;
     }
 }
