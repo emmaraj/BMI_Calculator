ffd8e3b8ac528f44a69ec156d3789716ee010a98
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-14 12:35:11 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         textViewBMI = findViewById(R.id.textViewBMI);
     }
 
-    public void onClickCalculate(View view){
+    public void onClickCalculate(View view) {
         String height_measure = spinnerHeight.getSelectedItem().toString().trim();
         String weight_measure = spinnerWeight.getSelectedItem().toString().trim();
 
@@ -40,25 +40,28 @@ public class MainActivity extends AppCompatActivity {
             double height = Double.parseDouble(heightValue);
             double weight = Double.parseDouble(weightValue);
 
-            if (isHeightValid(height) && isWeightValid(weight)){
+            if (isHeightValid(height) && isWeightValid(weight)) {
                 if (height_measure.equals("in")) {
                     height = convertInchesToMeters(height);
-                } else{
+                } else {
                     height = convertToMeters(height);
                 }
                 if (weight_measure.equals("lbs")) {
                     weight = convertToKilogram(weight);
                 }
                 //convert height into meters
-                textViewBMI.setText(calculateBMI(weight, height));
-            } else if (!isWeightValid(height)){
+
+                double BMI = calculateBMI(weight, height);
+                textViewBMI.setText(format("%.2f", BMI));
+                
+            } else if (!isWeightValid(height)) {
                 editTextHeight.setError("Kindly enter a valid Height");
             } else {
                 editTextWeight.setError("Kindly enter a valid Weight");
             }
         } else if (heightValue.length() == 0) {
             editTextHeight.setError("Kindly enter a value for Height");
-        } else{
+        } else {
             editTextWeight.setError("Kindly enter a value for Weight");
         }
 
@@ -69,43 +72,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *
      * @param inch Inputted Inch from the user
      * @return int Centimeter equivalent of an inch value
      */
 
-    private double convertInchesToMeters(double inch){
+    private double convertInchesToMeters(double inch) {
         return inch / 39.37;
     }
 
-    private double convertToKilogram(double value){
+    private double convertToKilogram(double value) {
         return value / 2.205;
     }
 
-    private String calculateBMI(double weight, double height) {
+    private double calculateBMI(double weight, double height) {
 //        DecimalFormat format = new DecimalFormat("##.00");
         double result = weight / Math.pow(height, 2);
-        Log.v("MyApp", "BMI: " + result);
-        return format("%.2f",result);
+        return result;
     }
 
-    private boolean isHeightValid(double height){
+    private boolean isHeightValid(double height) {
         boolean status = false;
 
-        if (height > 0.5){
+        if (height > 0.5) {
             status = true;
         }
 
         return status;
     }
 
-    private boolean isWeightValid(double weight){
+    private boolean isWeightValid(double weight) {
         boolean status = false;
 
-        if (weight > 5){
+        if (weight > 5) {
             status = true;
         }
 
         return status;
+    }
+
+    private String getClassification(double index) {
+        String classification = null;
+        if (index < 18.5) {
+            classification = "Underweight";
+        } else if (index >= 18.5 && index <= 24.9) {
+            classification = "Normal";
+        } else if (index >= 25.0 && index <= 29.9) {
+            classification = "Overweight";
+        } else if (index >= 30.0 && index <= 34.9) {
+            classification = "Obese Class I";
+        } else if (index >= 35.0 && index <= 39.9) {
+            classification = "Obese Class II";
+        } else if (index >= 40.0) {
+            classification = "Obese Class III";
+        }
+        return classification;
     }
 }
